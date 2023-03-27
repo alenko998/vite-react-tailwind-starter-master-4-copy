38f6ab988b0f1d0a2522a34d4519b3b84c6d9abd
alenko998 <alen_git@hotmail.com>
2023-03-27 14:03:11 +0200

    dd

--- src/routes/alen routes/CoursesPage.jsx
+++ src/routes/alen routes/CoursesPage.jsx
@@ -23,17 +23,7 @@ export default function CoursesPage() {
   },
   ]);
 
-  // const [filteredCourses, setFilteredCourses] = useState(courses);
 
-  // const [filter, setFilter] = useState({
-  //  beginner: false,
-  //  intermediate: false,
-  //  expert: false,
-  //  english: false,
-  //  espanol: false,
-  //  francais: false,
-  // },
-  // );
 
   const filters = [
     {
@@ -56,9 +46,6 @@ export default function CoursesPage() {
     },
   ]
 
-  const [filteredData, setFilteredData] = useState([]);
-  const [filterTerms, setFilterTerms] = useState([]);
-  const [nizJezika,setNizJezika] = useState(["english"]);
 
   useEffect(() => {
     fetch('http://localhost:3001/courses')
@@ -67,106 +54,10 @@ export default function CoursesPage() {
       })
       .then((data) => {
         setCourses(data);
-        setFilteredData(data);
       })
   }, []);
 
-  useEffect(() => {
-    // filter the data based on the filter terms
-    let filtered = courses;
-    for (let term in nizJezika) {
-      filtered = filtered.filter((item) =>
-        item.subtitles.toLowerCase().includes(term.toLowerCase())
-      );
-    }
-    setFilteredData(filtered);
-  }, [courses, filterTerms]);
 
-  console.log(filteredData);
-
-  const [filterCourse, setFilterCourse] = useState([]);
-
-  const [subtitleArray,setSubtitleArray] = useState([]);
-
-  const [levelArray,setLevelArray] = useState([]);
-
-  const [filterOptions, setFilterOptions] = useState([
-    {
-      id: "subtitles",
-      array: []
-    },
-    {
-      id: "level",
-      array: []
-    },
-  ])
-
-
-
-  
-
-  
-
-  useEffect(()=>{
- 
-    let finalData = [];
-    if(nizJezika.length == 0 && levelArray.length==0){
-      setFilterCourse(courses)
-    } else{
-      if(nizJezika.length > 0){
-        let newData = courses.filter(course => nizJezika.includes(course.subtitles))
-        finalData = finalData.concat(newData);
-     
-      }
-      if(levelArray.length > 0){
-        let newData = courses.filter(course => levelArray.includes(course.courseLevel))
-        finalData = finalData.concat(newData);
-        
-      }
-
-      setFilterCourse(finalData);
-      
-    }
-    
-  }, [courses,nizJezika])
-
-  // useEffect(() =>{
-  //   //ovo prati sve filtrirane iteme
-  //   let finalData = [];
-  //   //inicijalna vrednost filtriranih kurseva iliti svi kursevi
-  //   setFilteredCourses(courses)
-  //   //ifovi koji ti primene filtere
-  //   if(filter.beginner === true){
-  //     let newData = courses.filter(item => item.duration === 'beginner');
-  //     finalData = finalData.concat(newData);
-  //     setFilteredCourses(finalData);
-  //   }
-  //   if(filter.intermediate === true){
-  //     let newData = courses.filter(item => item.duration === 'intermediate');
-  //     finalData = finalData.concat(newData);
-  //     setFilteredCourses(finalData);
-  //   }
-  //   if(filter.expert === true){
-  //     let newData = courses.filter(item => item.duration === 'expert');
-  //     finalData = finalData.concat(newData);
-  //     setFilteredCourses(finalData);
-  //   }
-  //   if(filter.english === true){
-  //     let newData = courses.filter(item => item.subtitles === 'english');
-  //     finalData = finalData.concat(newData);
-  //     setFilteredCourses(finalData);
-  //   }
-  //   if(filter.espanol === true){
-  //     let newData = courses.filter(item => item.subtitles === 'espanol');
-  //     finalData = finalData.concat(newData);
-  //     setFilteredCourses(finalData);
-  //   }
-  //   if(filter.francais === true){
-  //     let newData = courses.filter(item => item.subtitles === 'francais');
-  //     finalData = finalData.concat(newData);
-  //     setFilteredCourses(finalData);
-  //   }
-  // },[filter, courses])
 
 
   const title = "My courses"
@@ -204,7 +95,7 @@ export default function CoursesPage() {
       <AlenSlideOver open={open} setOpen={setOpen} content={<AddCourseForm setOpen={setOpen}/>}/>
       <AlenSlideOver open={openEdit} setOpen={setOpenEdit} content={<EditCourseForm setOpen={setOpenEdit} idForDelete={idForDelete} setIdForDelete={setIdForDelete} course={course} setCourse={setCourse}/>}/>
       <AlenStackedlayout
-        content={<AlenFilterlayout filterOptions={filterOptions} setFilterOptions={setFilterOptions}  button={<PrimaryButton buttonName={addButton} setOpen={setOpen}/>} filters={filters} content={<CourseCard setIdForDelete={setIdForDelete} setCourse={setCourse} setOpenModal={setOpenModal} courses={courses} setOpen={setOpenEdit}/>} title={title} setOpen={setOpen}/>}/>
+        content={<AlenFilterlayout  button={<PrimaryButton buttonName={addButton} setOpen={setOpen}/>} filters={filters} content={<CourseCard setIdForDelete={setIdForDelete} setCourse={setCourse} setOpenModal={setOpenModal} courses={courses} setOpen={setOpenEdit}/>} title={title} setOpen={setOpen}/>}/>
     </div>
   )
 }
